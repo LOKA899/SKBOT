973117514c7cea585eac4703431467ccf630b016
LOKA899 <neel.puneeth@gmail.com>
2025-02-16 20:28:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     clientId: process.env.CLIENT_ID,
 
     // Command permissions
-    adminRoleId: '1339134803944935426',
+    adminRoleId: '1340690374389141627',
     moderatorRoleId: process.env.MODERATOR_ROLE_ID || '1339134803944935427',
     participantRoleId: process.env.PARTICIPANT_ROLE_ID || '1339134803944935428',
 
